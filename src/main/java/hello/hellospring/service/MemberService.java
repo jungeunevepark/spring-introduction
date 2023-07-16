7f5abe8f4b4fe09878c40a287a78a2c69b770c43
evecomcom98 <evecomcom98@naver.com>
2023-07-16 22:41:31 +0900

    :zap: AOP를 사용해야 하는 상황 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -19,13 +19,6 @@ public class MemberService {
     /*
     * 회원가입
     * */
-    @Transactional
-    public Long join(Member member){
-
-        validdateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
     private void validdateDuplicateMember(Member member){
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
@@ -33,11 +26,30 @@ public class MemberService {
                 });
     }
 
-    /*
-    * 전체 회원 조회
-    * */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+        try {
+            validdateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+    }
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
